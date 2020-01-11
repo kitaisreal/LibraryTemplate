4f0817949bfcc1145b28bd1f2bacf2d4749fde14
Kita, Maksim <kitaetoya@gmail.com>
2020-01-11 18:42:51 +0300

    Updated test target in Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
         .target(name: "LibraryTemplate", dependencies: [], path: "Sources"),
-        .testTarget(name: "LibrarySetupTests", dependencies: ["LibraryTemplate"], path: "Tests"),
+        .testTarget(name: "LibraryTemplateTests", dependencies: ["LibraryTemplate"], path: "Tests"),
     ],
     swiftLanguageVersions: [.v5]
 )
